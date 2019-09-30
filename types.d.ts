d4031aa32e560f16ee84e63684f27bb3e471bb08
Joshua Tzucker <joshua.tz@gmail.com>
2019-09-30 16:06:54 -0700

    fixed spaces -> tabs

--- types.d.ts
+++ types.d.ts
@@ -3,42 +3,42 @@
 type GitCommitHook = "pre" | "post" | "none";
 
 interface StampObject {
-    // the stamp of when the file was created
-    "created"?: number | boolean,
-    // the stamp of when the file was modified
-    "modified"?: number | boolean
+	// the stamp of when the file was created
+	"created"?: number | boolean,
+	// the stamp of when the file was modified
+	"modified"?: number | boolean
 }
 
 interface StampCache {
-    [index:string]: StampObject
+	[index:string]: StampObject
 }
 interface InputOptions {
-    // Whether or not the timestamps should be saved to file
-    outputToFile?: boolean,
-    // the filename to save the timestamps to
+	// Whether or not the timestamps should be saved to file
+	outputToFile?: boolean,
+	// the filename to save the timestamps to
 	outputFileName?: string,
 	// If the output file should automatically be check-in with git add
 	outputFileGitAdd?: boolean,
-    // Filenames to process
-    files?: string[] | string,
-    // Only update for files in these directories
-    onlyIn?: string[] | string,
-    // Block certain files from being tracked
+	// Filenames to process
+	files?: string[] | string,
+	// Only update for files in these directories
+	onlyIn?: string[] | string,
+	// Block certain files from being tracked
 	blockFiles?: string[] | string,
 	// Exception list of files that will override any blocks
 	allowFiles?: string[] | string,
-    // What triggered the execution
+	// What triggered the execution
 	gitCommitHook?: GitCommitHook,
 	// Project root
 	projectRootPath?: string
 }
 
 interface FinalizedOptions {
-    outputToFile: boolean,
+	outputToFile: boolean,
 	outputFileName?: string,
 	outputFileGitAdd?: boolean,
-    files: string[],
-    onlyIn?: string[],
+	files: string[],
+	onlyIn?: string[],
 	blockFiles?: string[],
 	allowFiles: string[],
 	gitCommitHook: GitCommitHook,
